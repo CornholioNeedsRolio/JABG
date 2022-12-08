9eb6cc2dbef67d853070eb4a56b96d9f4024e332
CornholioNeedsRolio <87880615+CornholioNeedsRolio@users.noreply.github.com>
2022-12-08 13:00:34 +0000

    Delete cmake-build-debug directory

--- cmake-build-debug/libs/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rellik/JABG/libs/glm/glm
-# Build directory: /home/rellik/JABG/cmake-build-debug/libs/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
